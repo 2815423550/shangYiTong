5ab086a60654a5f5aeca2ef79b1fb9f7c759c3d8
Toronto <Toronto@28154.com>
2022-09-23 14:17:01 +0800

    1

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author 黄华弢
+ * @date Created on 2022/9/23
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
