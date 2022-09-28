f7587ce59925ab97f6b8d8ca504936f483bdfa01
Toronto <Toronto@28154.com>
2022-09-28 18:43:16 +0800

    last

---
+++ yygh_parent/service/service_user/src/main/java/com/atguigu/yygh/user/controller/UserInfoApiController.java
@@ -0,0 +1,30 @@
+package com.atguigu.yygh.user.controller;
+
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.user.service.UserInfoService;
+import com.atguigu.yygh.vo.user.LoginVo;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserInfoApiController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @ApiOperation(value = "会员登录")
+    @PostMapping("login")
+    public Result login(@RequestBody LoginVo loginVo, HttpServletRequest request) {
+        // loginVo.setIp(IpUtil.getIpAddr(request));
+        Map<String, Object> info = userInfoService.loginUser(loginVo);
+        return Result.ok(info);
+    }
+}
